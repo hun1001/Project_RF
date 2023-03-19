6cbcb6e132aae3d433ce4cef93f2fe032d71e5db
seomin1210 <seoyun0700@daum.net>
2023-03-20 00:29:20 +0900

    스테이지 SO 생성, 스테이지 어떤식으로 할까 고민중

--- Assets/Scripts/Stage/Stage_Spawn.cs
+++ Assets/Scripts/Stage/Stage_Spawn.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pool;
 
 namespace Stage
 {
@@ -10,10 +11,27 @@ namespace Stage
         private StageListSO _stageListSO;
 
         private int _currentStage = 0;
+        private GameObject _currentMap;
+
+        private void StageStart()
+        {
+            _currentMap = RandomMapSelect();
+            Spawn();
+        }
 
         private void Spawn()
         {
+            for(int i = 0; i < _stageListSO.Stages[_currentStage].Enemys.Length; i++)
+            {
+                GameObject enemy = PoolManager.Get(_stageListSO.Stages[_currentStage].Enemys[i].name);
+                //enemy.transform.position =
+            }
+        }
 
+        private GameObject RandomMapSelect()
+        {
+            int randomIndex = Random.Range(0, _stageListSO.Maps.Length);
+            return _stageListSO.Maps[randomIndex];
         }
     }
 }
