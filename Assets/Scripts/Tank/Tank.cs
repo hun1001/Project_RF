f4535e91abf1b30f1cafc84540a786acbbcd85f1
hun1001 <l01022899266@gmail.com>
2023-03-20 12:07:49 +0900

    Revert "탱크 스크립트 다시 만드는중"
    This reverts commit e947d925196e98d44e0ec73c1688d05f6349088d.

--- Assets/Scripts/Tank/Tank.cs
+++ Assets/Scripts/Tank/Tank.cs
@@ -2,12 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[DisallowMultipleComponent]
 public class Tank : MonoBehaviour
 {
-    [Header("Stat")]
-    [SerializeField]
-    private TankStatSO _tankStatSO = null;
-
-    private Turret _turret = null;
+    // [SerializeField]
+    // private Turret _turret = null;
 }
