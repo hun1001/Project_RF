aff6eb9524997aaf18eae3bc9168f5e917756b62
seomin1210 <seoyun0700@daum.net>
2023-03-20 19:31:57 +0900

    아이템 리스트에 있는 아이템들을 가중치 리스트에 넣기, 아이템 코드 주석 추가

--- Assets/Scripts/Item/ItemListSO.cs
+++ Assets/Scripts/Item/ItemListSO.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,5 +7,7 @@ namespace Item
     [CreateAssetMenu(menuName = "SO/Item/ItemListSO")]
     public class ItemListSO : ScriptableObject
     {
+        /// <summary> 아이템 리스트 </summary>
+        public Item_Base[] ItemList;
     }
 }
--- Assets/Scripts/Item/ItemManager.cs
+++ Assets/Scripts/Item/ItemManager.cs
@@ -1,11 +1,28 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Util;
 
 namespace Item
 {
     public class ItemManager : MonoBehaviour
     {
-        
+        /// <summary> 아이템 리스트 SO </summary>
+        public ItemListSO ItemListSO;
+
+        /// <summary> 가중치 랜덤 뽑기 - 아이템 </summary>
+        private static WeightedRandomPicker<Item_Base> _picker = new WeightedRandomPicker<Item_Base>();
+
+        /// <summary> 아이템과 가중치 값을 넣는다 </summary>
+        private void Awake()
+        {
+            int weight = 0;
+            foreach(var item in ItemListSO.ItemList)
+            {
+                // y = -x + (1 + 희귀도 최대값)
+                weight = -item.ItemSO.Rarity + 6;
+                _picker.Add(item, weight);
+            }
+        }
     }
 }
