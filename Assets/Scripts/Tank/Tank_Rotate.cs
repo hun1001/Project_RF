eef5f199715971d7d8f921a728ff41b9ca04fe8c
hun1001 <l01022899266@gmail.com>
2023-03-20 20:08:25 +0900

    rotate 예비 코드

--- Assets/Scripts/Tank/Tank_Rotate.cs
+++ Assets/Scripts/Tank/Tank_Rotate.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class Tank_Rotate : Tank_Component
 {
-    // Start is called before the first frame update
-    void Start()
+    public void Rotate(Vector2 direction)
     {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
+        if (direction != Vector2.zero)
+        {
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
     }
 }
