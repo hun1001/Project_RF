02f44f4925f1a222057f104e5b3fa254504ed007
seomin1210 <seoyun0700@daum.net>
2023-03-21 14:58:10 +0900

    아이템 뽑기 중복으로 나오는거 방지

--- Assets/Scripts/Item/ItemManager.cs
+++ Assets/Scripts/Item/ItemManager.cs
@@ -10,7 +10,10 @@ namespace Item
         /// <summary> 아이템 리스트 SO </summary>
         public ItemListSO ItemListSO;
 
+        /// <summary> 가지고 있는 아이템 리스트 </summary>
         public Dictionary<Item_Base, int> HaveItemList = new Dictionary<Item_Base, int>();
+        /// <summary> 상점에 등장하고 있는 아이템 리스트 </summary>
+        private List<Item_Base> _showingItemList = new List<Item_Base>();
 
         /// <summary> 가중치 랜덤 뽑기 - 아이템 </summary>
         private WeightedRandomPicker<Item_Base> _picker = new WeightedRandomPicker<Item_Base>();
@@ -20,6 +23,7 @@ namespace Item
         {
             _picker.Clear();
             HaveItemList.Clear();
+            _showingItemList.Clear();
 
             int weight = 0;
             foreach(var item in ItemListSO.ItemList)
@@ -30,18 +34,32 @@ namespace Item
             }
         }
 
+        /// <summary> 아이템 뽑기 시작하는 함수 </summary>
+        public void ItemPickUp()
+        {
+            _showingItemList.Clear();
+            Item_Base item;
+            for(int i = 0; i < 3; i++)
+            {
+                while (true)
+                {
+                    item = _picker.GetRandomPick();
+                    if (_showingItemList.Contains(item) == false) break;
+                }
 
+                _showingItemList.Add(item);
+                Dummy(item);
+                
+                if(_picker.GetItemDictReadonly().Count == _showingItemList.Count)
+                {
+                    break;
+                }
+            }
+        }
 
         // 임시로 만든 함수
-        private void Dummy()
+        private void Dummy(Item_Base dummy)
         {
-            Item_Base dummy = ItemListSO.ItemList[0];
-
-            if (HaveItemList[dummy] == dummy.ItemSO.UpgradeMax - 1)
-            {
-                // 이미 최대 강화!
-                return;
-            }
             if (GoodsManager.DecreaseGoods(GoodsType.GameGoods, dummy.ItemSO.NecessaryGoods) == false)
             {
                 // 재화 부족!
