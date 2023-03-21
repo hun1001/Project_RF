0908d85318223b777f4d9c31f9e0ea683d5b7fca
hun1001 <l01022899266@gmail.com>
2023-03-21 12:04:41 +0900

    가속도 추가

--- Assets/Scripts/Tank/Tank_Move.cs
+++ Assets/Scripts/Tank/Tank_Move.cs
@@ -4,18 +4,55 @@ using UnityEngine;
 
 public class Tank_Move : Tank_Component
 {
-    private float _speed = 0f;
+    private float _maxSpeed = 0f;
+    private float _currentSpeed = 0f;
+    private float _acceleration = 0f;
+    private float _targetSpeed = 0f;
 
     private void Awake()
     {
-        _speed = Tank.TankStatSO.Speed;
+        _maxSpeed = Tank.TankStatSO.Speed;
+        _acceleration = Tank.TankStatSO.Acceleration;
     }
 
     public void Move(float magnitude)
     {
         if (magnitude > 0)
         {
-            transform.Translate(Vector3.up * magnitude * _speed * Time.deltaTime);
+            _targetSpeed = magnitude * _maxSpeed;
+
+            // if (_currentSpeed < _targetSpeed)
+            // {
+            //     _currentSpeed += _acceleration * Time.deltaTime;
+            // }
+            // else
+            // {
+            //     _currentSpeed = _targetSpeed;
+            // }
+
+            if (_currentSpeed != _targetSpeed)
+            {
+                _currentSpeed = Mathf.Lerp(_currentSpeed, _targetSpeed, _acceleration * Time.deltaTime);
+            }
+            else
+            {
+                _currentSpeed = _targetSpeed;
+            }
         }
+        else
+        {
+            _targetSpeed = 0;
+            _currentSpeed = 0;
+            // if (_currentSpeed > _targetSpeed)
+            // {
+            //     _currentSpeed -= _acceleration * Time.deltaTime;
+            // }
+            // else
+            // {
+            //     _currentSpeed = _targetSpeed;
+            // }
+        }
+
+        transform.Translate(Vector3.up * Time.deltaTime * _currentSpeed);
     }
 }
