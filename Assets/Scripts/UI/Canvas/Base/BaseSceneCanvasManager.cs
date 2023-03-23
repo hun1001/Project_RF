2dd5f1a487ba68e4e099bbaa39ac750ddd2b5f2e
hun1001 <l01022899266@gmail.com>
2023-03-23 12:46:36 +0900

    캔버스 관리자 생성

--- Assets/Scripts/UI/Canvas/Base/BaseSceneCanvasManager.cs
+++ Assets/Scripts/UI/Canvas/Base/BaseSceneCanvasManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [DisallowMultipleComponent]
-public abstract class BaseCanvas : MonoBehaviour
+public abstract class BaseSceneCanvasManager : MonoBehaviour
 {
 
 }
