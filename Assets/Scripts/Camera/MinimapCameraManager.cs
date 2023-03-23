f40a36ad62f3a4c06b234fc05840420649efdd97
seomin1210 <seoyun0700@daum.net>
2023-03-23 20:16:27 +0900

    미니맵 중간 커밋

--- Assets/Scripts/Camera/MinimapCameraManager.cs
+++ Assets/Scripts/Camera/MinimapCameraManager.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class MinimapCameraManager : MonoBehaviour
 {
+    private Camera _cam;
+    private Vector2 _size;
+
+    private void Start()
+    {
+        TryGetComponent(out _cam);
+        _size = new Vector2(_cam.orthographicSize, _cam.orthographicSize * _cam.aspect);
+    }
+
     void Update()
     {
         transform.rotation = Quaternion.identity;
