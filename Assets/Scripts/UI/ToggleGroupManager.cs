5b08e64e137dc571d7d7be4c7b2fa877f7310070
hun1001 <l01022899266@gmail.com>
2023-03-24 12:18:00 +0900

    UI 동적으로 생성되게 변경

--- Assets/Scripts/UI/ToggleGroupManager.cs
+++ Assets/Scripts/UI/ToggleGroupManager.cs
@@ -1,9 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Events;
 
 public class ToggleGroupManager : ToggleGroup
 {
+    private static Toggle _templateToggle = null;
 
+    public void SetToggleGroup(string[] toggleText, UnityAction<bool>[] onValueChanged, int onIndex = 0)
+    {
+        _templateToggle ??= transform.GetChild(0).GetComponent<Toggle>();
+        _templateToggle.gameObject.SetActive(false);
+
+        for (int i = 0; i < toggleText.Length; ++i)
+        {
+            Toggle toggle = null;
+
+            toggle = Instantiate(_templateToggle, transform);
+            toggle.gameObject.SetActive(true);
+
+            toggle.transform.GetChild(1).GetComponent<Text>().text = toggleText[i];
+            toggle.onValueChanged.RemoveAllListeners();
+            toggle.onValueChanged.AddListener(onValueChanged[i]);
+            toggle.group = this;
+            toggle.isOn = i == onIndex;
+        }
+    }
 }
