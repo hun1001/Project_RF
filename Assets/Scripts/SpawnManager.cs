c990c99881fb3be8a65e7027ec55323034861bd3
hun1001 <l01022899266@gmail.com>
2023-03-28 12:31:28 +0900

    스폰매니저 제작중

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pool;
 
 public class SpawnManager : MonoBehaviour
 {
-    private Dictionary<GroupType, List<GameObject>> _spawnedUnits = new Dictionary<GroupType, List<GameObject>>();
+    private Dictionary<GroupType, List<CustomObject>> _spawnedUnits = new Dictionary<GroupType, List<CustomObject>>();
 
-    public void SpawnUnit(GameObject unit, Vector3 position, Quaternion rotation)
+    public void SpawnUnit(CustomObject unit, Vector3 position, Quaternion rotation, GroupType groupType)
     {
-
+        var u = PoolManager.Get(unit.ID, position, rotation);
     }
 }
