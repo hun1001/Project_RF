8c66b6f2fc9ee234fc17c838a060ac29fb4de4fb
seomin1210 <seoyun0700@daum.net>
2023-03-30 00:06:05 +0900

    메뉴씬 탱크 구경하기 추가

---
+++ Assets/Scripts/Camera/MenuSceneCameraManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using UnityEngine.EventSystems;
+
+public class MenuSceneCameraManager : MonoBehaviour
+{
+    private CinemachineFreeLook _cam;
+
+    private void Awake()
+    {
+        TryGetComponent(out _cam);
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && EventSystem.current.IsPointerOverGameObject() == false)
+        {
+            _cam.m_XAxis.m_InputAxisName = "Mouse X";
+            _cam.m_YAxis.m_InputAxisName = "Mouse Y";
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            _cam.m_XAxis.m_InputAxisName = "";
+            _cam.m_YAxis.m_InputAxisName = "";
+
+            _cam.m_XAxis.m_InputAxisValue = 0f;
+            _cam.m_YAxis.m_InputAxisValue = 0f;
+        }
+    }
+}
