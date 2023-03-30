54de3d04d809fc1a055e85e43cd2f9fb83642aa2
hun1001 <l01022899266@gmail.com>
2023-03-30 20:17:22 +0900

    so 가져오는 editor 제작중

--- Assets/Editor/SheetDataToSO/SheetDataToSO.cs
+++ Assets/Editor/SheetDataToSO/SheetDataToSO.cs
@@ -1,14 +1,19 @@
+using System.Net;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEditor;
 
 public class SheetDataToSO : EditorWindow
 {
     private string _sheetURL = "";
+    // https://docs.google.com/spreadsheets/d/1Sph3_eEfKFAfOT_EEN2-XzM9RK7mrly_9FTFSueqgSo/export?format=tsv 탱크
 
     private string[] _dataType = new string[] { "Tank", "Turret", "Shell" };
     private int _dataTypeIndex = 0;
+
+    private const string _sheetAPI = "export?format=tsv";
 
     [MenuItem("Window/SheetDataToSO")]
     static void Init()
@@ -21,7 +26,11 @@ public class SheetDataToSO : EditorWindow
     {
         _sheetURL = EditorGUILayout.TextField("Sheet URL", _sheetURL);
 
+        GUILayout.Space(10);
+
         _dataTypeIndex = EditorGUILayout.Popup("Data Type", selectedIndex: _dataTypeIndex, displayedOptions: _dataType);
+
+        GUILayout.Space(10);
 
         if (GUILayout.Button("Load"))
         {
@@ -34,7 +43,28 @@ public class SheetDataToSO : EditorWindow
         switch (_dataType[_dataTypeIndex])
         {
             case "Tank":
-                Debug.Log("Tank");
+                UnityWebRequest www = UnityWebRequest.Get(_sheetURL + _sheetAPI);
+                www.SendWebRequest();
+
+                while (!www.isDone) { }
+
+                string result = www.downloadHandler.text;
+                string result2 = result.Replace("\r", "");
+
+                string[] lines = result2.Split('\n');
+
+                Debug.Log(result);
+
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    string[] datas = lines[i].Split('\t');
+
+                    for (int j = 0; j < datas.Length; j++)
+                    {
+                        //  Debug.Log(datas[j]);
+                    }
+                }
+
                 break;
             case "Turret":
                 Debug.Log("Turret");
