061d67d74fb8fb108f6a056ef885da1d789e56e0
hun1001 <l01022899266@gmail.com>
2023-03-31 12:07:07 +0900

    터렛 데이터까지 가져옴 그러나 Shell은 아직 안만듬

--- Assets/Editor/SheetDataToSO/SheetDataToSO.cs
+++ Assets/Editor/SheetDataToSO/SheetDataToSO.cs
@@ -10,6 +10,7 @@ public class SheetDataToSO : EditorWindow
 {
     private string _sheetURL = "";
     // https://docs.google.com/spreadsheets/d/1Sph3_eEfKFAfOT_EEN2-XzM9RK7mrly_9FTFSueqgSo/ 탱크
+    // https://docs.google.com/spreadsheets/d/1mUDMYbdVgwLQDmMQb2mB6kYl4SzZdbGfOmiaQ9Ww0g4/ 터렛
 
     private string[] _dataType = new string[] { "Tank", "Turret", "Shell" };
     private int _dataTypeIndex = 0;
@@ -55,50 +56,65 @@ public class SheetDataToSO : EditorWindow
     {
         ResetFolder();
 
+        UnityWebRequest www = UnityWebRequest.Get(_sheetURL + _sheetAPI);
+        www.SendWebRequest();
+
+        while (!www.isDone) { }
+
+        string result = www.downloadHandler.text;
+        string result2 = result.Replace("\r", "");
+
+        string[] lines = result2.Split('\n');
+
         switch (_dataType[_dataTypeIndex])
         {
             case "Tank":
-                UnityWebRequest www = UnityWebRequest.Get(_sheetURL + _sheetAPI);
-                www.SendWebRequest();
-
-                while (!www.isDone) { }
-
-                string result = www.downloadHandler.text;
-                string result2 = result.Replace("\r", "");
-
-                string[] lines = result2.Split('\n');
-
-
                 for (int i = 1; i < lines.Length; i++)
                 {
                     string[] data = lines[i].Split('\t');
 
                     for (int j = 0; j < data.Length; j++)
                     {
-                        TankSO tankSO = ScriptableObject.CreateInstance<TankSO>();
+                        TankSO asset = ScriptableObject.CreateInstance<TankSO>();
 
-                        tankSO.Acceleration = float.Parse(data[1]);
+                        asset.Acceleration = float.Parse(data[1]);
                         data[2] = data[2].Replace("km/h", "");
-                        tankSO.MaxSpeed = float.Parse(data[2]);
-                        tankSO.RotationSpeed = float.Parse(data[3]);
-                        tankSO.Armour = float.Parse(data[4]);
-                        tankSO.HP = float.Parse(data[5]);
-                        tankSO.TankType = TankType.Medium;
+                        asset.MaxSpeed = float.Parse(data[2]);
+                        asset.RotationSpeed = float.Parse(data[3]);
+                        asset.Armour = float.Parse(data[4]);
+                        asset.HP = float.Parse(data[5]);
+                        asset.TankType = TankType.Medium;
 
-                        AssetDatabase.CreateAsset(tankSO, "Assets/ScriptableObjects/Tank/" + data[0].ToString() + "_TankSO.asset");
+                        AssetDatabase.CreateAsset(asset, "Assets/ScriptableObjects/Tank/" + data[0].ToString() + "_TankSO.asset");
                         AssetDatabase.SaveAssets();
 
                         EditorUtility.FocusProjectWindow();
-                        //Selection.activeObject = tankSO;
                     }
                 }
-
                 break;
             case "Turret":
-                Debug.Log("Turret");
+                for (int i = 1; i < lines.Length; i++)
+                {
+                    string[] data = lines[i].Split('\t');
+
+                    for (int j = 0; j < data.Length; j++)
+                    {
+                        TurretSO asset = ScriptableObject.CreateInstance<TurretSO>();
+
+                        asset.Power = float.Parse(data[1]);
+                        asset.ReloadTime = float.Parse(data[2]);
+                        data[3] = data[3].Replace("deg/s", "");
+                        asset.RotationSpeed = float.Parse(data[3]);
+
+                        AssetDatabase.CreateAsset(asset, "Assets/ScriptableObjects/Turret/" + data[0].ToString() + "_TurretSO.asset");
+                        AssetDatabase.SaveAssets();
+
+                        EditorUtility.FocusProjectWindow();
+                    }
+                }
                 break;
             case "Shell":
-                Debug.Log("Shell");
+                Debug.Log("Shell is not implemented yet.");
                 break;
             default:
                 break;
@@ -118,5 +134,7 @@ public class SheetDataToSO : EditorWindow
         {
             di.Delete(true);
         }
+
+        AssetDatabase.Refresh();
     }
 }
