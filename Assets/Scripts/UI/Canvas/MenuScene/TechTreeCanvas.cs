a3d31a672df44948695c4f342ea2b44d19e9ac10
hun1001 <l01022899266@gmail.com>
2023-04-04 16:42:10 +0900

    techtree 제작중 UI 바꿔서

--- Assets/Scripts/UI/Canvas/MenuScene/TechTreeCanvas.cs
+++ Assets/Scripts/UI/Canvas/MenuScene/TechTreeCanvas.cs
@@ -7,5 +7,6 @@ public class TechTreeCanvas : BaseCanvas
     [SerializeField]
     private TechTree _techTree = null;
 
-
+    [SerializeField]
+    private ToggleGroupManager _toggleGroupManager = null;
 }
