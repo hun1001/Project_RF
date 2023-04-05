f39e1bdb72e74be38bf082b1e0f23f511986ba5a
hun1001 <l01022899266@gmail.com>
2023-04-05 20:07:26 +0900

    조준선 색 변경

--- Assets/Scripts/Tank/Turret/Turret_AimLine.cs
+++ Assets/Scripts/Tank/Turret/Turret_AimLine.cs
@@ -8,9 +8,53 @@ public class Turret_AimLine : Turret_Component
 {
     private LineRenderer _lineRenderer = null;
 
+    private Gradient[] _gradients = new Gradient[3];
+
     private void Awake()
     {
         _lineRenderer = GetComponent<LineRenderer>();
+
+        _gradients[0] = new Gradient()
+        {
+            colorKeys = new GradientColorKey[]
+            {
+                new GradientColorKey(Color.red, 0f),
+                new GradientColorKey(Color.red, 1f)
+            },
+            alphaKeys = new GradientAlphaKey[]
+            {
+                new GradientAlphaKey(1f, 0f),
+                new GradientAlphaKey(1f, 1f)
+            }
+        };
+
+        _gradients[1] = new Gradient()
+        {
+            colorKeys = new GradientColorKey[]
+            {
+                new GradientColorKey(Color.yellow, 0f),
+                new GradientColorKey(Color.yellow, 1f)
+            },
+            alphaKeys = new GradientAlphaKey[]
+            {
+                new GradientAlphaKey(1f, 0f),
+                new GradientAlphaKey(1f, 1f)
+            }
+        };
+
+        _gradients[2] = new Gradient()
+        {
+            colorKeys = new GradientColorKey[]
+            {
+                new GradientColorKey(Color.green, 0f),
+                new GradientColorKey(Color.green, 1f)
+            },
+            alphaKeys = new GradientAlphaKey[]
+            {
+                new GradientAlphaKey(1f, 0f),
+                new GradientAlphaKey(1f, 1f)
+            }
+        };
     }
 
     private void Update()
@@ -18,17 +62,30 @@ public class Turret_AimLine : Turret_Component
         // TODO : 총알 존재 시간 2f -> 총알 속도 * 총알 존재 시간
         _lineRenderer.SetPosition(0, Turret.FirePoint.position);
 
-        var rayData = Physics2D.Raycast(Turret.FirePoint.position, Turret.FirePoint.up, Turret.CurrentShell.Speed * 2f);
+        var a = Turret.GetComponent<Turret_Attack>(ComponentType.Attack);
 
-        if (rayData.collider != null && rayData.collider != Turret.GetComponent<Tank>().GetComponent<Collider2D>())
+        if (a.ReloadingTime <= 0f)
         {
-            _lineRenderer.SetPosition(1, rayData.point + (Vector2)Turret.FirePoint.up);
+            var rayData = Physics2D.Raycast(Turret.FirePoint.position, Turret.FirePoint.up, Turret.CurrentShell.Speed * 2f);
+
+            if (rayData.collider != null && rayData.collider != Turret.GetComponent<Tank>().GetComponent<Collider2D>())
+            {
+                _lineRenderer.SetPosition(1, rayData.point + (Vector2)Turret.FirePoint.up);
+                if (rayData.collider.gameObject.layer == LayerMask.NameToLayer("Tank"))
+                {
+                    _lineRenderer.colorGradient = _gradients[2];
+                }
+            }
+            else
+            {
+                _lineRenderer.SetPosition(1, Turret.FirePoint.position + Turret.FirePoint.up * Turret.CurrentShell.Speed * 2f);
+                _lineRenderer.colorGradient = _gradients[1];
+            }
         }
         else
         {
             _lineRenderer.SetPosition(1, Turret.FirePoint.position + Turret.FirePoint.up * Turret.CurrentShell.Speed * 2f);
+            _lineRenderer.colorGradient = _gradients[0];
         }
-
-        _lineRenderer.renderingLayerMask = Turret.GetComponent<Turret_Attack>().ReloadingTime > 0 ? 0u : 1u;
     }
 }
