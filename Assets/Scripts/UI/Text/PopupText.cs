5084dd6334ae273c24700f0f6826fdbe6c7d4d43
seomin1210 <seoyun0700@daum.net>
2023-04-06 13:00:33 +0900

    데미지 텍스트 밑작업

--- Assets/Scripts/UI/Text/PopupText.cs
+++ Assets/Scripts/UI/Text/PopupText.cs
@@ -1,8 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
 
-public class PopupText : MonoBehaviour
+public class PopupText : MonoBehaviour, IPoolReset
 {
-    
+    [SerializeField]
+    private Text _text;
+
+    public void SetText(string text)
+    {
+        _text.text = text;
+    }
+
+    public void PoolObjectReset()
+    {
+        _text.text = null;
+    }
+
+    public void SetText(int text) => SetText(text.ToString());
+    public void SetText(float text) => SetText(text.ToString());
 }
