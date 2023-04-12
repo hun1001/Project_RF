49fcb7d26a0302df8866a28a440901e205636b93
hun1001 <l01022899266@gmail.com>
2023-04-12 20:24:47 +0900

    탱크 생성 에디터 제작중

--- Client/Assets/Editor/TankGenerator/TankGenerator.cs
+++ Client/Assets/Editor/TankGenerator/TankGenerator.cs
@@ -5,10 +5,33 @@ using UnityEditor;
 
 public class TankGenerator : EditorWindow
 {
+    private CountryType _countryType = CountryType.None;
+    private GameObject _tankModel = null;
+
     [MenuItem("Tools/Tank Generator")]
     static void Init()
     {
         TankGenerator window = (TankGenerator)EditorWindow.GetWindow(typeof(TankGenerator));
         window.Show();
+    }
+
+    private void OnGUI()
+    {
+        _countryType = (CountryType)EditorGUILayout.EnumPopup("Country Type", _countryType);
+
+        _tankModel = (GameObject)EditorGUILayout.ObjectField("Tank Model", _tankModel, typeof(GameObject), false);
+
+        GUILayout.Space(10);
+
+        if (GUILayout.Button("Generate"))
+        {
+            GenerateTank();
+        }
+    }
+
+    private void GenerateTank()
+    {
+
+        Debug.Log("Generate Tank");
     }
 }
