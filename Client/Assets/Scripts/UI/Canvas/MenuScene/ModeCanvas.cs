c886cb8870615dac3969658b5afdadfb622dcd1d
seomin1210 <seoyun0700@daum.net>
2023-04-13 12:57:40 +0900

    모드 UI 애니메이션 수정

--- Client/Assets/Scripts/UI/Canvas/MenuScene/ModeCanvas.cs
+++ Client/Assets/Scripts/UI/Canvas/MenuScene/ModeCanvas.cs
@@ -6,6 +6,11 @@ using DG.Tweening;
 
 public class ModeCanvas : BaseCanvas
 {
+    [Header("Modes")]
+    [SerializeField]
+    private RectTransform _modesFrame;
+    private Image[] _modeImages;
+
     [Header("Maps")]
     [SerializeField]
     private RectTransform _content;
@@ -17,12 +22,29 @@ public class ModeCanvas : BaseCanvas
     private void Awake()
     {
         _mapImages = _content.GetComponentsInChildren<Image>();
+        _modeImages = _modesFrame.GetComponentsInChildren<Image>();
     }
 
     private void Start()
     {
         _startSequence = DOTween.Sequence()
         .SetAutoKill(false)
+        .PrependCallback(() =>
+        {
+            foreach(Image image in _modeImages)
+            {
+                image.DOFade(0f, 0f);
+            }
+        })
+        .Prepend(_modesFrame.DOAnchorPosX(-200f, 0f))
+        .Append(_modesFrame.DOAnchorPosX(48f, 0.5f))
+        .InsertCallback(0.25f, () =>
+        { 
+            foreach(Image image in _modeImages)
+            {
+                image.DOFade(1f, 0.5f);
+            }
+        })
         .PrependCallback(() =>
         {
             _scrollView.anchoredPosition += Vector2.right * 600f;
@@ -32,7 +54,7 @@ public class ModeCanvas : BaseCanvas
             }
         })
         .Append(_scrollView.DOAnchorPosX(0f, 1.2f))
-        .InsertCallback(0.2f, () =>
+        .InsertCallback(0.7f, () =>
         {
             foreach (Image image in _mapImages)
             {
