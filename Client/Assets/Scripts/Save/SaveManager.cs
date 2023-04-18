6c115b15826eac03f0a984f30c333beb46ba13e8
hun1001 <l01022899266@gmail.com>
2023-04-18 11:07:27 +0900

    저장 기능 제작 테스트 필요

---
+++ Client/Assets/Scripts/Save/SaveManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public static class SaveManager
+{
+    private static readonly string _savePath = Application.persistentDataPath + "/save/";
+
+    public static void Save<T>(string key, T data)
+    {
+        if (!Directory.Exists(_savePath))
+        {
+            Directory.CreateDirectory(_savePath);
+        }
+
+        string json = JsonUtility.ToJson(data);
+        File.WriteAllText(_savePath + key + ".json", json);
+    }
+
+    public static T Load<T>(string key)
+    {
+        if (!Directory.Exists(_savePath))
+        {
+            Directory.CreateDirectory(_savePath);
+        }
+
+        string json = File.ReadAllText(_savePath + key + ".json");
+        return JsonUtility.FromJson<T>(json);
+    }
+}
