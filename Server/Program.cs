8b95e5a5d3510bc8b98728563ff577506252b178
hun1001 <l01022899266@gmail.com>
2023-04-27 11:15:42 +0900

    서버 제작중

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 namespace Server;
 
@@ -7,6 +8,26 @@ internal static class Program
 {
     private static void Main(string[] args)
     {
-        
+        TcpListener tcpListener = new(IPAddress.Parse("127.0.0.1"), 7777);
+
+        tcpListener.Start();
+
+        while (true)
+        {
+            TcpClient tcpClient = tcpListener.AcceptTcpClient();
+
+            NetworkStream networkStream = tcpClient.GetStream();
+
+            byte[] buffer = new byte[1024];
+
+            int bytes = networkStream.Read(buffer, 0, buffer.Length);
+
+            string message = Encoding.UTF8.GetString(buffer, 0, bytes);
+
+            Console.WriteLine(message);
+
+            networkStream.Close();
+            tcpClient.Close();
+        }
     }
 }
