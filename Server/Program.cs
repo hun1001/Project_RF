2597af46682977144d4086028f6379f45b7534c6
hun1001 <l01022899266@gmail.com>
2023-04-27 11:04:30 +0900

    server massive edit

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,113 +1,12 @@
-﻿using System;
-using System.Threading;
-using System.Net;
+﻿using System.Net;
 using System.Net.Sockets;
-using System.Text;
 
 namespace Server;
-static class Program
+
+internal static class Program
 {
-    public static Dictionary<int, ClientHandle> clientsDictionary = new();
-    private static int userCnt = 0;
-    private static object lockSocket = new();
-
-    private static TcpListener serverSocket = null;
-
-    static void Main()
+    private static void Main(string[] args)
     {
-        try
-        {
-            serverSocket = new TcpListener(IPAddress.Any, 7777);
-            
-            TcpClient clientSocket;
-            byte[] bytesFrom = new byte[1024];
-            string dataFromClient = "";
-
-            serverSocket.Start(); 
-            Console.WriteLine("Server Started...");
-
-            while (true)
-            {
-                clientSocket = serverSocket.AcceptTcpClient();
-                dataFromClient = "";
-
-                ClientHandle client = new ClientHandle();
-                clientsDictionary.Add(userCnt, client);
-
-                client.StartClient(clientSocket, userCnt);
-
-                ++userCnt;
-            }
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine(ex.ToString());
-        }
-    }
-
-    public static TcpClient GetSocket(int id)
-    {
-        TcpClient socket = null;
-        if (clientsDictionary.ContainsKey(id))
-        {
-            ClientHandle hc = clientsDictionary[id];
-            socket = hc.clientSocket;
-        }
-        return socket;
-    }
-
-    public static void Broadcast(Packet packet)
-    {
-        byte[] data = packet.Serialize();
         
-        lock (lockSocket)
-        {
-            foreach (KeyValuePair<int, ClientHandle> client in clientsDictionary)
-            {
-                TcpClient socket = client.Value.clientSocket;
-                NetworkStream networkStream = socket.GetStream();
-
-                try
-                {
-                    networkStream.Write(data, 0, data.Length);
-                    networkStream.Flush();
-                }
-                catch(Exception e)
-                {
-                    socket.Close();
-                    networkStream.Close();
-                    Console.WriteLine(e.ToString());
-                }
-            }
-        }
-    }
-
-    public static void UserAdd(string clientNo)
-    {
-        string memberIDList = "";
-
-        lock (lockSocket)
-        {
-            foreach (KeyValuePair<int, ClientHandle> client in clientsDictionary)
-            {
-                memberIDList += client.Value.clientID + ",";
-            }
-        }
-
-        Broadcast(new Packet("", Command.COMMAND_ENTER, memberIDList));
-    }
-
-    public static void UserLeft(int userID, string clientNo)
-    {
-        if (clientsDictionary.ContainsKey(userID))
-        {
-            Broadcast(new Packet("id", Command.COMMAND_LEFT, ""));
-            Console.WriteLine("Client Left: " + clientNo);
-
-            TcpClient clientSocket = GetSocket(userID);
-
-            clientsDictionary.Remove(userID);
-            clientSocket.Close();
-        }
     }
 }
