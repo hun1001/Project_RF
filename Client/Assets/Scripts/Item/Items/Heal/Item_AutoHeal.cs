d26663e33663258b812f025fae10187d9c0acba3
seomin1210 <seoyun0700@daum.net>
2023-04-27 10:33:26 +0900

    아이템 구조 수정중 - 업그레이드 삭제

--- Client/Assets/Scripts/Item/Items/Heal/Item_AutoHeal.cs
+++ Client/Assets/Scripts/Item/Items/Heal/Item_AutoHeal.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class Item_AutoHeal : Item.Item_Base
 {
-    [SerializeField]
-    private float[] _upgradeHeal;
     private float _currentHeal;
 
     private Tank_Damage _tankDamage;
@@ -13,18 +11,11 @@ public class Item_AutoHeal : Item.Item_Base
 
     protected override void CreateItem()
     {
-        _currentHeal = _upgradeHeal[0];
         transform.parent.TryGetComponent(out _tankDamage);
 
         _healDelay = new WaitForSeconds(1f);
         StartCoroutine(Heal());
     }
-
-    protected override void UpgradeItem()
-    {
-        //_currentHeal = _upgradeHeal[Item.ItemManager.Instance.HaveItemList[this]];
-    }
-
     private IEnumerator Heal()
     {
         while (true)
--- Client/Assets/Scripts/Item/Items/Heal/Item_Recycling.cs
+++ Client/Assets/Scripts/Item/Items/Heal/Item_Recycling.cs
@@ -5,23 +5,15 @@ using Event;
 
 public class Item_Recycling : Item.Item_Base
 {
-    [SerializeField]
-    private float[] _upgradeHeal;
     protected float _currentHeal;
 
     protected Tank_Damage _tankDamage;
 
     protected override void CreateItem()
     {
-        _currentHeal = _upgradeHeal[0];
         transform.parent.TryGetComponent(out _tankDamage);
 
         AddEvent();
-    }
-
-    protected override void UpgradeItem()
-    {
-        //_currentHeal = _upgradeHeal[Item.ItemManager.Instance.HaveItemList[this]];
     }
 
     protected virtual void AddEvent()
