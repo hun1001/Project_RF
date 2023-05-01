74b07095d3be92aac8aa16fd6a7ff3df44c47c59
seomin1210 <seoyun0700@daum.net>
2023-05-01 20:14:37 +0900

    아이템 장착 데이터 저장

--- Client/Assets/Scripts/Save/SaveKey.cs
+++ Client/Assets/Scripts/Save/SaveKey.cs
@@ -6,5 +6,6 @@ public static class SaveKey
 {
     public static string GetTechTreeProgress(CountryType countryType) => countryType.ToString() + "_TechTreeProgress";
     public static string GetItemInventory(ItemType itemType) => itemType.ToString() + "_ItemInventory";
+    public static string GetItemEquipment(ItemType itemType) => itemType.ToString() + "_ItemEquipment";
     public const string GoodsInformation = "GoodsInformation";
 }
