e5fe0a4d483254102af526dfe25c899e1485900b
seomin1210 <seoyun0700@daum.net>
2023-05-18 12:59:42 +0900

    재장전 시간 UI 일단 추가

--- Client/Assets/Scripts/UI/Canvas/GameScene/InformationCanvas.cs
+++ Client/Assets/Scripts/UI/Canvas/GameScene/InformationCanvas.cs
@@ -8,4 +8,35 @@ public class InformationCanvas : BaseCanvas
     [SerializeField]
     private Bar _hpBar = null;
     public Bar HpBar => _hpBar;
+
+    [SerializeField]
+    private Image _reloadImage = null;
+
+    private Player _player;
+    private Turret _playerTurret;
+
+    private void Awake()
+    {
+        _reloadImage.gameObject.SetActive(false);
+        _player = FindObjectOfType<Player>();
+        Tank pt = _player.Tank;
+        _playerTurret = pt.Turret;
+        _playerTurret.GetComponent<Turret_Attack>().AddOnFireAction(() => StartCoroutine(ReloadCheck())); 
+    }
+
+    private IEnumerator ReloadCheck()
+    {
+        float reloadTime = _playerTurret.TurretSO.ReloadTime;
+        float currentTime = 0;
+        _reloadImage.gameObject.SetActive(true);
+
+        while (currentTime < reloadTime)
+        {
+            _reloadImage.fillAmount = currentTime / reloadTime;
+            currentTime += Time.deltaTime;
+            yield return null;
+        }
+
+        _reloadImage.gameObject.SetActive(false);
+    }
 }
