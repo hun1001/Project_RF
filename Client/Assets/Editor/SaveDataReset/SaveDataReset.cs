fb85df7286bf417e54f1df369b1cc34e85c87e86
hun1001 <l01022899266@gmail.com>
2023-05-31 18:58:48 +0900

    버그 해결 완료!

--- Client/Assets/Editor/SaveDataReset/SaveDataReset.cs
+++ Client/Assets/Editor/SaveDataReset/SaveDataReset.cs
@@ -1,11 +1,14 @@
 using UnityEditor;
 using System.IO;
+using UnityEngine;
 
 public class SaveDataReset
 {
     [MenuItem("Tools/SaveDataReset")]
     static void Init()
     {
+        PlayerPrefs.DeleteAll();
+
         DirectoryInfo di = new DirectoryInfo(SaveManager.SavePath);
 
         foreach (FileInfo file in di.GetFiles())
