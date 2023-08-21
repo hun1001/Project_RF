27f43221d25a9a643cc465107b5b9b228de44438
hun1001 <l01022899266@gmail.com>
2023-08-21 19:56:06 +0900

    wave 세팅중

--- Client/Assets/Scripts/Map/Map_Information.cs
+++ Client/Assets/Scripts/Map/Map_Information.cs
@@ -9,16 +9,14 @@ namespace Map
         [SerializeField]
         private Transform _spawnPointParent;
 
-        /// <summary> 적 생성 위치들 </summary>
-        private Transform[] _spawnPoints => _spawnPointParent.GetComponentsInChildren<Transform>().Where(x => x != _spawnPointParent).ToArray();
-        public Transform[] SpawnPoints => _spawnPoints;
+        public Transform[] SpawnPoints => _spawnPointParent.GetComponentsInChildren<Transform>().Where(x => x != _spawnPointParent).ToArray();
 
         /// <summary> 적 생성 위치를 랜덤으로 반환하는 함수 </summary>
         /// <returns> 적 생성 위치 </returns>
         public Vector3 RandomSpawnPoint()
         {
-            int randomIndex = Random.Range(0, _spawnPoints.Length);
-            return _spawnPoints[randomIndex].position;
+            int randomIndex = Random.Range(0, SpawnPoints.Length);
+            return SpawnPoints[randomIndex].position;
         }
     }
 }
