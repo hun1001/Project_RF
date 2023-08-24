5554ae3ee9c8cde5f5c9abf1c16c5f95e48a80ed
hun1001 <l01022899266@gmail.com>
2023-08-24 19:31:37 +0900

    리페어팩 기능 제작

--- Client/Assets/Scripts/Item/RepairPack.cs
+++ Client/Assets/Scripts/Item/RepairPack.cs
@@ -1,11 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pool;
 
 public class RepairPack : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log(collision.name);
+        var tank = collision.GetComponent<Tank>();
+
+        if (tank != null)
+        {
+            Debug.Log("RepairPack");
+            PoolManager.Pool("RepairPack", gameObject);
+
+            tank.GetComponent<Tank_Damage>(ComponentType.Damage).Repair(20);
+        }
     }
 }
