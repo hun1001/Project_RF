1aeba7579fd15d4efe7728885f9c2a6ca0d63c9c
seomin1210 <seoyun0700@daum.net>
2023-08-24 20:24:51 +0900

    튜토리얼 업뎃

--- Client/Assets/Scripts/UI/Canvas/MenuScene/TutorialCanvas.cs
+++ Client/Assets/Scripts/UI/Canvas/MenuScene/TutorialCanvas.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.Collections.LowLevel.Unsafe;
 using UnityEngine;
 
 public class TutorialCanvas : BaseCanvas
@@ -17,10 +18,26 @@ public class TutorialCanvas : BaseCanvas
     [SerializeField]
     private GameObject[] _tutorialPanels;
 
+    [Header("Menu UI")]
+    [SerializeField]
+    private GameObject _menuUI = null;
+    [SerializeField]
+    private GameObject _shellList = null;
+    [SerializeField]
+    private GameObject _filter = null;
+
+    [Header("TechTree UI")]
+    [SerializeField]
+    private GameObject _techTreeUI = null;
+
     private int _tutorialCount = 0;
 
     private void Awake()
     {
+        _menuUI.SetActive(true);
+        _shellList.SetActive(false);
+        _filter.SetActive(false);
+        _techTreeUI.SetActive(false);
         _tutorialPanelParent.SetActive(false);
         _skipPanel.SetActive(true);
 
@@ -56,6 +73,7 @@ public class TutorialCanvas : BaseCanvas
 
     public void TutorialStart()
     {
+        _tutorialCount = 0;
         _tutorialPanels[0].SetActive(true);
         _tutorialText.SetText(_textsSO.TutorialTexts[0]);
     }
@@ -65,5 +83,22 @@ public class TutorialCanvas : BaseCanvas
         _tutorialPanels[_tutorialCount++].SetActive(false);
         _tutorialText.SetText(_textsSO.TutorialTexts[_tutorialCount]);
         _tutorialPanels[_tutorialCount].SetActive(true);
+
+        if (_tutorialCount == 1234)
+        {
+            _shellList.SetActive(true);
+        }
+        if (_tutorialCount == 12345)
+        {
+            _filter.SetActive(true);
+        }
+        if (_shellList.activeSelf)
+        {
+            _shellList.SetActive(false);
+        }
+        if (_filter.activeSelf)
+        {
+            _filter.SetActive(false);
+        }
     }
 }
