e283c48f704113b3b251d9fe226cbc8e3e017e6d
hun1001 <l01022899266@gmail.com>
2023-08-24 12:01:23 +0900

    보스 프리펩 추가

--- Client/Assets/Scripts/GameWay/Wave/WaveManager.cs
+++ Client/Assets/Scripts/GameWay/Wave/WaveManager.cs
@@ -13,17 +13,14 @@ public class WaveManager : GameWay_Base
 
     protected override void Spawn()
     {
-        //for (int i = 0; i < (_repeatCnt * 0.25) + 1; i++)
         if (StageListSO.Stages[CurrentStage].IsBoss)
-        {
-            // 보스 웨이브 전용 로직 작동
-        }
-        else
         {
             
         }
-
-        base.Spawn();
+        else
+        {
+            base.Spawn();
+        }
     }
 
     public override void StageClear()
