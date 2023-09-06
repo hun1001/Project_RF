bb919c4d394f13b67a606048a8fe6045ebb5e035
hun1001 <l01022899266@gmail.com>
2023-09-06 18:35:16 +0900

    Tip 수정 빌드 버그 팁 안뜨는거 수정중

--- Client/Assets/Scripts/LoadingScene/TipManager.cs
+++ Client/Assets/Scripts/LoadingScene/TipManager.cs
@@ -11,6 +11,7 @@ public class TipManager : MonoBehaviour
     private Text _tipText = null;
 
     private List<TipSO> tipSOList = new List<TipSO>();
+    private List<string> beforeTipText = new List<string>();
 
     private void Awake()
     {
@@ -29,6 +30,22 @@ public class TipManager : MonoBehaviour
 
     private void SetRandomTipText()
     {
-        _tipText.text = tipSOList[Random.Range(0, tipSOList.Count)].tipText;
+        _tipText.text = GetRandomTipText();
+    }
+
+    private string GetRandomTipText()
+    {
+        string randomTipText = tipSOList[Random.Range(0, tipSOList.Count)].tipText;
+        while (beforeTipText.Contains(randomTipText))
+        {
+            randomTipText = tipSOList[Random.Range(0, tipSOList.Count)].tipText;
+        }
+        beforeTipText.Add(randomTipText);
+        if (beforeTipText.Count > 5)
+        {
+            beforeTipText.RemoveAt(0);
+        }
+        Debug.Log("TipText: " + randomTipText);
+        return randomTipText;
     }
 }
