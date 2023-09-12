2dab9f05d6b9de319155d6b519f69495c978637b
hun1001 <l01022899266@gmail.com>
2023-09-12 09:49:55 +0900

    수리펙 애니메이션 추가

--- Client/Assets/Scripts/RepairPack.cs
+++ Client/Assets/Scripts/RepairPack.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Pool;
+using DG.Tweening;
 
 public class RepairPack : MonoBehaviour
 {
@@ -17,5 +18,10 @@ public class RepairPack : MonoBehaviour
                 PoolManager.Pool("RepairPack", gameObject);
             }
         }
+    }
+
+    private void OnEnable()
+    {
+        transform.DORotate(new Vector3(0, 0, 360), 2f, RotateMode.FastBeyond360).SetLoops(-1).SetEase(Ease.Linear);
     }
 }
