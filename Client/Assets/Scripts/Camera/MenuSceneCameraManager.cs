5f35bfc4ff926c84c57228cb49b5d81fdf69ad53
snrnsrk5 <77601112+snrnsrk5@users.noreply.github.com>
2023-09-15 22:48:41 +0900

    카메라 시야각
    ㅇㅇ

--- Client/Assets/Scripts/Camera/MenuSceneCameraManager.cs
+++ Client/Assets/Scripts/Camera/MenuSceneCameraManager.cs
@@ -45,7 +45,7 @@ public class MenuSceneCameraManager : MonoBehaviour
         if (_isOnUI == false)
         {
             _cam.m_Lens.FieldOfView += -Input.GetAxis("Mouse ScrollWheel") * 10;
-            _cam.m_Lens.FieldOfView = Mathf.Clamp(_cam.m_Lens.FieldOfView, 15, 40);
+            _cam.m_Lens.FieldOfView = Mathf.Clamp(_cam.m_Lens.FieldOfView, 30, 75);
         }
     }
 }
