1fab6129263e2f40b96a3b309f4fcaf2d05928c8
hun1001 <l01022899266@gmail.com>
2023-09-19 11:57:25 +0900

    약점

--- Client/Assets/Scripts/Tank/Boss/ChurchillCrocodileBoss/ChurchillCrocodileBossTank.cs
+++ Client/Assets/Scripts/Tank/Boss/ChurchillCrocodileBoss/ChurchillCrocodileBossTank.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pool;
 
 public class ChurchillCrocodileBossTank : BossTank
 {
     public override string AI_Address => "ChurchillCrocodileBoss_AI";
 
-    [SerializeField]
-    private GameObject _weakness = null;
+    private ChurchillCrocodileBossWeakness _weakness = null;
 
     private void Start()
     {
-        _weakness = Instantiate(_weakness, new Vector3(0, -4.16f, -1.568f), Quaternion.identity);
+        _weakness = PoolManager.Get<ChurchillCrocodileBossWeakness>("Crocodile_Weakness", transform);
+        _weakness.transform.localPosition = new Vector3(0, 0, -1.568f);
+        _weakness.transform.SetParent(null);
+        _weakness.Setting(GetComponent<Rigidbody2D>());
     }
 }
