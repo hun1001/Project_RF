7319dcf5371ac058276e93cc8f49d770455c5fbe
hun1001 <l01022899266@gmail.com>
2023-09-19 19:29:00 +0900

    카메라 줌 아웃 제한 값 수정

--- Client/Assets/Scripts/Camera/CameraManager.cs
+++ Client/Assets/Scripts/Camera/CameraManager.cs
@@ -93,7 +93,7 @@ public class CameraManager : MonoBehaviour
     public void CameraZoom(float fovChange)
     {
         _virtualCamera.m_Lens.FieldOfView += fovChange;
-        _virtualCamera.m_Lens.FieldOfView = Mathf.Clamp(_virtualCamera.m_Lens.FieldOfView, 30, 60);
+        _virtualCamera.m_Lens.FieldOfView = Mathf.Clamp(_virtualCamera.m_Lens.FieldOfView, 30, 75);
         _virtualCamera.GetCinemachineComponent<CinemachineTransposer>().m_FollowOffset.z = -_virtualCamera.m_Lens.FieldOfView;
     }
 
