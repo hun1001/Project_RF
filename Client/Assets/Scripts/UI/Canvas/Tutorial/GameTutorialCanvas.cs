1118d50bb20a1bd08cf7ae384e281148b737ec28
seomin1210 <seoyun0700@daum.net>
2023-09-20 10:14:26 +0900

    게임 튜토 클릭 사운드 추가

--- Client/Assets/Scripts/UI/Canvas/Tutorial/GameTutorialCanvas.cs
+++ Client/Assets/Scripts/UI/Canvas/Tutorial/GameTutorialCanvas.cs
@@ -212,6 +212,7 @@ public class GameTutorialCanvas : BaseCanvas
             return;
         }
 
+        PlayButtonSound();
         _textDuration = _textsSO.TutorialTexts[_tutorialCount].Length * 0.025f;
         _tutorialText.Typing(_textsSO.TutorialTexts[_tutorialCount], _textDuration);
         _textButton.SetActive(true);
